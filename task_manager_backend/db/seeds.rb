ba9e62366fba87849dacaff09f57b4e8fa7565ca
soncrayon <arnold.curtis@gmail.com>
2021-07-12 23:22:10 -0400

    add some project and milestone seed data

--- task_manager_backend/db/seeds.rb
+++ task_manager_backend/db/seeds.rb
@@ -7,3 +7,11 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'faker'
 
+5.times do
+    Project.create(name: Faker::GreekPhilosophers.name, client: Faker::Company.name)
+end
+
+Milestone.create([
+    {name: "Collection", description: "Collecting data for processing", project_id: 1},
+    {name: "Processing", description: "Process data in database", project_id: 2}
+])
